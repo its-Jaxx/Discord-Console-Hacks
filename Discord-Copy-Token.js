cc12d09df6ca0cf5ebe1c790d890592e2427b413
jaxx <96382604+its-Jaxx@users.noreply.github.com>
2023-02-20 10:33:02 -0800

    Create Discord-Copy-Token.js
    This allows you to automatically copy your Discord token to your clipboard.
    Note: NEVER SHARE YOUR DISCORD TOKEN WITH ANYONE!

---
+++ Discord-Copy-Token.js
@@ -0,0 +1,1 @@
+window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getToken !== undefined) {return copy(m.default.getToken())}if (m.getToken !== undefined) {return copy(m.getToken())}}}]);
