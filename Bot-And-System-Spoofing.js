89c908eda0c2195987c7c77a349ecceb048cbd5b
jaxx <96382604+its-Jaxx@users.noreply.github.com>
2023-02-21 10:15:29 -0800

    Update Bot-And-System-Spoofing.js

--- Bot-And-System-Spoofing.js
+++ Bot-And-System-Spoofing.js
@@ -1,6 +1,3 @@
-//This will give you certain tags that are normally not obtainable.
-//Note: This is only visible for you.
-
 //Bot Tag
 window.webpackChunkdiscord_app.push([[Math.random()], {}, (req) => {for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {if (m.default && m.default.getCurrentUser !== undefined) {return m.default.getCurrentUser().bot = true;}if (m.getCurrentUser !== undefined) {return m.getCurrentUser().bot = true}}}])
 
