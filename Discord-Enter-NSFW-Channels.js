84325ed512f12f4ecf0bf936481d0f5046e0d228
jaxx <96382604+its-Jaxx@users.noreply.github.com>
2023-03-15 12:49:11 -0700

    Delete Discord-Enter-NSFW-Channels.js

--- Discord-Enter-NSFW-Channels.js
+++
@@ -1,2 +0,0 @@
-var findModule=(item)=>window.webpackChunkdiscord_app.push([[Math.random()],{},(req)=>{for(const m of Object.keys(req.c).map((x)=>req.c[x].exports).filter((x)=>x)){if(m.default&&m.default[item]!==undefined)return m.default}}])
-findModule('getCurrentUser').getCurrentUser().nsfwAllowed = true
